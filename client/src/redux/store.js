9d947b7d54d319a1072f645d7d8df68236912df4
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-03-27 15:39:43 +0530

    feat: add redux-persist for state persistence in Redux store configuration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,26 +1,29 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage"; // Local storage
 import { persistReducer, persistStore } from "redux-persist";
 
+// Root reducer combining slices
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
 const persistConfig = {
-    key: 'root',
-    storage,
-    version: 1,
+  key: "root",
+  storage,  // Using local storage
+  version: 1,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Configuring the store with persisted reducer
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false, // Allowing non-serializable values like persisted state
     }),
 });
 
+// Creating persistor for the store
 export const persistor = persistStore(store);
