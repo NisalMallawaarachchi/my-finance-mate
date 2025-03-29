197ae00cb1b059b16eacd5a23b3ab82f5c889cce
isirafernandzz <isirafernandoe@gmail.com>
2025-03-29 05:34:11 +0530

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    hmr: {
+      overlay: false,
+    },
     proxy: {
       "/api": {
         target: "http://localhost:3000",
