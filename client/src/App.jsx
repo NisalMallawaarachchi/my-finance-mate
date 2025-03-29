eb638e21f1ed621f8c348bdf94ee38a79d2b46e2
isirafernandzz <isirafernandoe@gmail.com>
2025-03-29 05:34:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes, Route, useLocation, } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes, useLocation, } from "react-router-dom";
 import About from "./pages/About";
+import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
-import Dashboard from "./pages/Dashboard";
+import OfferDescription from "./components/OfferDescription";
+import Offers from "./components/Offers";
 import Sidebar from "./components/Sidebar";
+import SubOffers from "./components/SubOffers";
 import Budget from "./pages/Budget";
+import Dashboard from "./pages/Dashboard";
+import Expense from "./pages/Expense";
 
 
 export default function App() {
@@ -23,12 +27,18 @@ function AppContent() {
 
   return (
     <div className="flex">
-     
-{(location.pathname === "/dashboard" || location.pathname === "/budget" ) && <Sidebar />}
+
+      {(location.pathname === "/dashboard" ||
+        location.pathname === "/budget" ||
+        location.pathname === "/expense" ||
+        location.pathname === "/offers" ||
+        location.pathname === "/offers/:category" ||
+        location.pathname === "/offers/:category/:subOffer"
+      ) && <Sidebar />}
 
 
       <div className="flex-1">
-        
+
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -38,7 +48,10 @@ function AppContent() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/budget" element={<Budget />} />
-
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/offers/:category" element={<SubOffers />} />
+          <Route path="/offers/:category/:subOffer" element={<OfferDescription />} />
         </Routes>
       </div>
     </div>
